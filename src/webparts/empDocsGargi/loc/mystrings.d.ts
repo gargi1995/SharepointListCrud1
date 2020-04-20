7c5bd634f7a1186e95dfffd374853008a821cce2
Gargi Dutta <gargi20dutta@gmail.com>
2020-04-20 20:23:02 +0530

    commit

---
+++ src/webparts/empDocsGargi/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IEmpDocsGargiWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'EmpDocsGargiWebPartStrings' {
+  const strings: IEmpDocsGargiWebPartStrings;
+  export = strings;
+}
